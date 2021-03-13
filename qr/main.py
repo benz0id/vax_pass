b129e8cb6ff5a57261723bcb243cb40081805fdb
bfern <benjamin.tudorprice@mail.utoronto.ca>
2021-03-13 17:36:16 -0500

    test 1

--- qr/main.py
+++ qr/main.py
@@ -3,6 +3,6 @@ from vax_qr_generator import *
 
 vax, first, last = user_input()
 qr = QR_Generator()
-qr.generate_simple_qr(vax, first + " " + last, 0, os.path.join(""))
+qr.generate_simple_qr(vax, first + " " + last, 0, os.path.join("qr_images"))
 #change
 
