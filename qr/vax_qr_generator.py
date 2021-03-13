6e62136095bf6138f0e2f1bd06ea900d4a03bc44
bfern <benjamin.tudorprice@mail.utoronto.ca>
2021-03-13 17:11:43 -0500

    test 1

--- qr/vax_qr_generator.py
+++ qr/vax_qr_generator.py
@@ -1,14 +1,34 @@
 import qrcode
 from PIL import Image
+from qrcode.image.pure import PymagingImage
+import os.path
 
 class QR_Generator:
     """A class designed to encode QR codes that transmit and interpret vaccine
     verification info.
     """
+    qr: qrcode.QRCode
 
     def __init__(self):
-        pass
+        self.qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_H,
+            box_size=10,
+            border=4,
+        )
 
-    def generate_simple_qr(self, name: str, userid: int):
+    def generate_simple_qr(self, name: str, userid: int, dest: str):
         """Generates an unencrypted QR code containing the <name> and <userid>.
+        Stores the QR code as a png in <dest>.
         """
+        dest_path = os.path.join(dest, "qrcode.png")
+        self.qr.add_data(''.join([name, ':', str(userid)]))
+        self.qr.make(fit=True)
+        img = self.qr.make_image(fill_color="black", back_color="white")
+        self.qr.clear()
+        img.save(dest_path)
+
+
+
+
+
