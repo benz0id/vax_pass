618681b7c9727fdd24aa90e1955463508f0d99a0
Evan Attfield <evanattfield@gmail.com>
2021-03-13 17:20:07 -0500

    function def

--- qr/input.py
+++ qr/input.py
@@ -1,12 +1,15 @@
-vax_input = input("Have you been vaccinated? (Yes or No): ")
-vax = False
-if vax_input == "Yes":
-    vax = True
-first_name = input("What is your first name?: ")
-last_name = input("What is your last name?: ")
-
-if vax == True:
-    print("{} {} has been vaccinated.".format(first_name, last_name))
-else:
-    print("{} {} has not been vaccinated.".format(first_name, last_name))
+def input():
+    vax_input = input("Have you been vaccinated? (Yes or No): ")
+    vax = False
+    if vax_input == "Yes":
+        vax = True
+    first_name = input("What is your first name?: ")
+    last_name = input("What is your last name?: ")
     
+    if vax == True:
+        print("{} {} has been vaccinated.".format(first_name, last_name))
+    else:
+        print("{} {} has not been vaccinated.".format(first_name, last_name))
+    
+    return vax, first_name, last_name
+        
