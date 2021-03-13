e9ea6b53813953862aec92c935fc6fd1ba9740f5
Evan Attfield <evanattfield@gmail.com>
2021-03-13 17:01:02 -0500

    basic inputs

--- qr/input.py
+++ qr/input.py
@@ -1,1 +1,12 @@
-#test
+vax_input = input("Have you been vaccinated? (Yes or No): ")
+vax = False
+if vax_input == "Yes":
+    vax = True
+first_name = input("What is your first name?: ")
+last_name = input("What is your last name?: ")
+
+if vax == True:
+    print("{} {} has been vaccinated.".format(first_name, last_name))
+else:
+    print("{} {} has not been vaccinated.".format(first_name, last_name))
+    
