96f3272454c35ae7913a254b909f62464c725f10
Evan Attfield <evanattfield@gmail.com>
2021-03-13 18:25:52 -0500

    added date of birth

--- qr/input.py
+++ qr/input.py
@@ -1,17 +1,26 @@
 def user_input():
     vax_input = input("Have you been vaccinated? (Yes or No): ")
+    assert vax_input == "Yes" or vax_input == "No", \
+        "Input must be 'Yes' or 'No'"
+    
     vax = False
     if vax_input == "Yes":
         vax = True
     first_name = input("What is your first name?: ")
     last_name = input("What is your last name?: ")
     
+    
+    birthday = int(input("What is your date of birth? (YYYYMMDD): "))
+    assert birthday >= 10000000 and birthday <= 99999999, \
+        "Date of birth must be valid"
+    
+    
     if vax == True:
         print("{} {} has been vaccinated.".format(first_name, last_name))
     else:
         print("{} {} has not been vaccinated.".format(first_name, last_name))
     
-    return vax, first_name, last_name
-#change
+    return vax, first_name, last_name, birthday
+#TODO date of birth
 
         
--- qr/main.py
+++ qr/main.py
@@ -1,9 +1,9 @@
 from input import *
 from vax_qr_generator import *
 
-vax, first, last = user_input()
+vax, first, last, birthday = user_input()
 qr = QRGenerator()
-qr.generate_simple_qr(vax, first + " " + last, 0, os.path.join(
+qr.generate_simple_qr(vax, first, last, birthday, 0, os.path.join(
     "qr_images_output"))
 #change
 
