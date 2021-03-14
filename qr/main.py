d9f699c342c61b6147c136d1ffd3f5e2c45ebee6
Evan Attfield <evanattfield@gmail.com>
2021-03-14 00:14:08 -0500

    Version 1.0

--- qr/main.py
+++ qr/main.py
@@ -1,5 +1,6 @@
 from input import *
 from vax_qr_generator import *
+from vax_qr_reader import *
 from user import *
 
 "vax, first, last, birthday = user_input()"
@@ -10,5 +11,11 @@ print(user)
 
 qr = QRGenerator()
 qr.generate_simple_qr(user, os.path.join("qr_images_output"))
-#change
+
+reader = QRReader()
+reader_data = reader.read_qr(os.path.join("qr_images_output", "qrcode.png"))
+
+user2 = User()
+user2.user_from_qr(reader_data)
+print(user2)
 
