94a724b03619a4594920e28f831184c744c82f92
Evan Attfield <evanattfield@gmail.com>
2021-03-13 19:09:44 -0500

    user stuff

--- qr/main.py
+++ qr/main.py
@@ -1,9 +1,15 @@
 from input import *
 from vax_qr_generator import *
+from user import *
 
-vax, first, last, birthday = user_input()
-qr = QRGenerator()
+"vax, first, last, birthday = user_input()"
+
+user = User()
+user.create_user()
+print(user.get_user_string)
+
+'''qr = QRGenerator()
 qr.generate_simple_qr(vax, first, last, birthday, 0, os.path.join(
-    "qr_images_output"))
+    "qr_images_output"))'''
 #change
 
--- qr/user.py
+++ qr/user.py
@@ -19,7 +19,7 @@ class User:
 
     def get_user_string(self) -> str:
         """Returns a succinct representation of the users attributes."""
-        birthday_string = String(self._birthday)
+        birthday_string = str(self._birthday)
         birthday_string = birthday_string[0:4] + "/" + birthday_string[4:6] + "/" + birthday_string[6:8]
         
         vax_string = "Not Vaccinated"
@@ -33,10 +33,10 @@ Vaccination Status: {}
         
         return user_string #TODO
     
-    def create_user():
+    def create_user(self):
         # asks vaccination status (must be 'Yes' or 'No')
         vax_input = ""
-        while vax_input != "Yes" or vax_input != "No":
+        while vax_input != "Yes" and vax_input != "No":
             vax_input = input("Have you been vaccinated? (Yes or No): ")
         
         # converts to Boolean
@@ -50,7 +50,7 @@ Vaccination Status: {}
         self._last_name = input("What is your last name?: ")
         
         # gets birthday (must be 8 digits)
-        while self._birthday < 10000000 or self._birthday > 99999999:
+        while self._birthday < 10000000 and self._birthday > 99999999:
             self._birthday = int(input("What is your date of birth? (YYYYMMDD): "))
         
         
