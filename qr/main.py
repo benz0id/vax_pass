791bed02bfb97068e00514865d8cf2c203913cb0
bfern <benjamin.tudorprice@mail.utoronto.ca>
2021-03-14 11:58:54 -0400

    added option to scan from camera

--- qr/main.py
+++ qr/main.py
@@ -4,6 +4,7 @@ from vax_qr_reader import *
 from user import *
 
 "vax, first, last, birthday = user_input()"
+reader = QRReader()
 
 user = User()
 user.user_signup()
@@ -11,7 +12,6 @@ user.user_signup()
 qr = QRGenerator()
 qr.generate_simple_qr(user, os.path.join("qr_images_output"))
 
-reader = QRReader()
 reader_data = reader.read_qr(os.path.join("qr_images_output", "qrcode.png"))
 
 user2 = User()
