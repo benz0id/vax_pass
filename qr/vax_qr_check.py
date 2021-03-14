a03cd8d6daba8a803da44243990f216791b35e05
Evan Attfield <evanattfield@gmail.com>
2021-03-14 01:19:05 -0500

    build check

--- qr/vax_qr_check.py
+++ qr/vax_qr_check.py
@@ -1,1 +1,13 @@
-#checker
+from vax_qr_reader import *
+#from user import *
+
+class QRCheck:
+    
+    def __init__(self):
+        self.reader = QRReader()
+
+    def check_db(self, folder, file):
+        reader_data = self.reader.read_qr(os.path.join(folder, file))
+
+        data = reader_data.split(":")
+
